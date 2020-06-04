d893635ea78351d28ebc4025b461ac7de07c4607
uwaheed88 <usman.waheed@datopian.com>
2020-06-04 07:46:30 -0700

    Improvement in the code and isolating I/O from logic in migrator

--- migrate_metedata/migrator.py
+++ migrate_metedata/migrator.py
@@ -14,32 +14,38 @@ class Migrator:
 
     def __init__(self, ckan_classic_api, new_metastore, options):
         self.ckan_classic_api = ckan_classic_api
-        self.new_metastore = new_metastore
-        self.config = options
+        self.metastore_client = store.create_metastore(new_metastore, **options)
 
     def migrate_dataset(self, pkg_name):
-        urlpath = self.ckan_classic_api + '/package_show?id=' + pkg_name
-
+        urlpath = self.create_url('/package_show?id=', pkg_name)
         response = json.loads(urllib.urlopen(urlpath).read())
-        pkg_dict = response['result']
-
-        package_id = pkg_dict["id"]
-        data_package_json = converter.dataset_to_datapackage(pkg_dict)
-
-        try:
-            metastore_client = store.create_metastore(self.new_metastore, **self.config)
-            package_info = metastore_client.create(package_id, data_package_json)
-        except Exception as e:
-            log.info("{} dataset is already exists".format(pkg_name))
+        self.create_pkg(response, pkg_name)
 
     def migrate_all_datasets(self):
-        urlpath = self.ckan_classic_api + '/package_list'
+        urlpath = self.create_url('/package_list')
+        response = json.loads(urllib.urlopen(urlpath).read())
+        self.iterate_pkgs(response)
 
-        try:
-            response = json.loads(urllib.urlopen(urlpath).read())
-            pkgs_list = response['result']
-        except ValueError as e:
-            log.info("There is no dataset available in your CKAN")
+    def create_url(self, suffix, pkg_name=''):
+        url = self.ckan_classic_api + suffix
+        url += pkg_name
+        return url
+
+    def create_pkg(self, data, pkg_name):
+        pkg_dict = data.get('result')
+
+        if pkg_dict:
+            data_package_json = converter.dataset_to_datapackage(pkg_dict)
+
+            try:
+                package_info = self.metastore_client.create(pkg_name, data_package_json)
+            except store.exc.Conflict as e:
+                log.info("{} dataset already exists".format(pkg_name))
+
+    def iterate_pkgs(self, data):
+        pkgs_list = data.get('result')
+        if not pkgs_list:
+            raise ValueError("There is no dataset available in your CKAN")
 
         for pkg_name in pkgs_list:
             self.migrate_dataset(pkg_name)
